2abb6f8134092a4ef6bb79bc6f5dc8f1d9c9caa3
rani1029 <ranig1029@gmail.com>
2021-08-07 11:56:11 +0530

    welcome msg oops

---
+++ LineCompOops/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LineCompOops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to Line Comparision Program 2");
+        }
+    }
+}
